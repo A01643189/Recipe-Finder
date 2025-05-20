18c5622f082406344e655d5e3258d8ce6b5a040d
A01643189 <a01643189@tec.mx>
2025-05-20 13:25:53 -0600

    fix profile css

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,12 +1,14 @@
 export default function Profile() {
-    return (
-        <div className="profile">
-            <h1 className="profile-title">Your Profile</h1>
-            <p className="profile-description">
-                This is your profile page. You’ll be able to manage your
-                account, view saved recipes, and update your preferences here in
-                the future.
-            </p>
-        </div>
-    );
+  return (
+    <div className="profile">
+      <h1 className="profile-title">Your Profile</h1>
+
+      <div className="profile-card">
+        <p className="profile-description">
+          This is your profile page. You’ll be able to manage your account, view
+          saved recipes, and update your preferences here in the future.
+        </p>
+      </div>
+    </div>
+  );
 }
